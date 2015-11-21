e4d547b5b7f0212e0469f1646768f4a6b4524f4c
Kevin Bache <kevin@bache.ca>
2015-11-21 18:49:37 -0500

    wrapper for number

--- src/app/gamelibrary/draw-stones.js
+++ src/app/gamelibrary/draw-stones.js
@@ -11,7 +11,7 @@
                 seeds = seeds.concat("<li class=\"seed\"></li>");
             }
             seeds = seeds.concat('</ul>');
-            return stones === 0 ? '' : seeds + stones.toString();
+            return stones === 0 ? '' : seeds + "<div class=\"seeds-number\">" +stones.toString() + "</div>";
         };
 
         this.current_player_store.innerHTML = format(this.mancala.current_store);
@@ -29,13 +29,13 @@
      */
     Game.prototype.draw_stones = function (pit) {
         var format = function (stones) {
-             var seeds = "<ul>";
+           var seeds = "<ul>";
             for(var seed = 0; seed < stones; seed ++) {
                 seeds = seeds.concat("<li class=\"seed\"></li>");
             }
             seeds = seeds.concat('</ul>');
-            return stones === 0 ? '' : seeds + stones.toString();
-        };
+            return stones === 0 ? '' : seeds + "<div class=\"seeds-number\">" +stones.toString() + "</div>";
+         };
 
         if (pit === this.pits) {
             this.current_player_store.innerHTML = format(this.mancala.current_store);
