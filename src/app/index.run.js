09a91d0b94ffd07f6aba3548402be8e99fb04535
churel <alicewarnier@macbook-pro-de-alice-warnier.local>
2015-11-21 11:49:14 -0500

    First message

---
+++ src/app/index.run.js
@@ -0,0 +1,14 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('mancala')
+    .run(runBlock);
+
+  /** @ngInject */
+  function runBlock($log) {
+
+    $log.debug('runBlock end');
+  }
+
+})();
